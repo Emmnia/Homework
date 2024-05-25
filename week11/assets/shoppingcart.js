961387667e994c5275698f97224432f3a29a679f
Emmnia <eugmalikova@gmail.com>
2024-05-25 16:37:00 +0300

    cart

--- week11/assets/shoppingcart.js
+++ week11/assets/shoppingcart.js
@@ -1,14 +1,22 @@
 const button = document.getElementById('discount');
 const result = document.getElementById('result');
-const price1 = parseFloat(document.getElementById('price1').innerHTML);
-const price2 = parseFloat(document.getElementById('price2').innerHTML);
-const price3 = parseFloat(document.getElementById('price3').innerHTML);
-const price4 = parseFloat(document.getElementById('price4').innerHTML);
+const price1 = document.getElementById('price1');
+const price2 = document.getElementById('price2');
+const price3 = document.getElementById('price3');
+const price4 = document.getElementById('price4');
 
-result.textContent = price1 + price2 + price3 + price4 + '₽';
+result.textContent = parseFloat(price1.innerHTML) + parseFloat(price2.innerHTML) + parseFloat(price3.innerHTML) + parseFloat(price4.innerHTML) + '₽';
 
 function applyDiscount() {
-    let discountPrice = (price1 + price2 + price3 + price4)*0.8;
+    let newPrice1 = parseFloat(price1.innerHTML)*0.8;
+    let newPrice2 = parseFloat(price2.innerHTML)*0.8;
+    let newPrice3 = parseFloat(price3.innerHTML)*0.8;
+    let newPrice4 = parseFloat(price4.innerHTML)*0.8;
+    let discountPrice = newPrice1 + newPrice2 + newPrice3 + newPrice4;
+    price1.textContent = newPrice1;
+    price2.textContent = newPrice2;
+    price3.textContent = newPrice3;
+    price4.textContent = newPrice4;
     result.textContent = discountPrice.toFixed(1) + '₽';
     button.disabled = true;
 }
