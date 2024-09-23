a2bd0f572f78f018a33447f82875d894f9cb6cba
Emmnia <eugmalikova@gmail.com>
2024-09-23 21:50:00 +0300

    fix switch

--- week23/tariff/src/components/Tariff/Tariff.jsx
+++ week23/tariff/src/components/Tariff/Tariff.jsx
@@ -2,31 +2,31 @@ import './Tariff.css'
 
 export const Tariff = ({ price, speed }) => {
     let priceValue = price
-    let nameSection
-    let priceSection
+    let nameClass
+    let priceClass
     switch (priceValue) {
         case 300:
-            nameSection = <div className="name name_blue"><h2>Безлимитный {price}</h2></div>;
-            priceSection = <div className="price price_blue"><p><sup>руб</sup> <span className="price-number">{price}</span> <sub>/мес</sub></p></div>;
+            nameClass = "name_blue";
+            priceClass = "price_blue";
             break;
         case 450:
-            nameSection = <div className="name name_green"><h2>Безлимитный {price}</h2></div>;
-            priceSection = <div className="price price_green"><p><sup>руб</sup> <span className="price-number">{price}</span> <sub>/мес</sub></p></div>;
+            nameClass = "name_green";
+            priceClass = "price_green";
             break;
         case 550:
-            nameSection = <div className="name name_red"><h2>Безлимитный {price}</h2></div>;
-            priceSection = <div className="price price_red"><p><sup>руб</sup> <span className="price-number">{price}</span> <sub>/мес</sub></p></div>;
+            nameClass = "name_red";
+            priceClass = "price_red";
             break;
         case 1000:
-            nameSection = <div className="name name_black"><h2>Безлимитный {price}</h2></div>;
-            priceSection = <div className="price price_black"><p><sup>руб</sup> <span className="price-number">{price}</span> <sub>/мес</sub></p></div>;
+            nameClass = "name_black";
+            priceClass = "price_black";
             break;
     }
 
     return (
         <div className={"card " + (priceValue == 550 ? "main" : "")}>
-            {nameSection}
-            {priceSection}
+            <div className={"name " + nameClass}><h2>Безлимитный {price}</h2></div>
+            <div className={"name " + priceClass}><p><sup>руб</sup> <span className="price-number">{price}</span> <sub>/мес</sub></p></div>
             <div className="speed"><p>до {speed} Мбит/сек</p></div>
             <div className="traffic"><p>Объем включенного трафика не ограничен</p></div>
         </div>
